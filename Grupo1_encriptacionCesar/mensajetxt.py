077caaf32dea15851159a4c51ae4c2e900aed498
Juan Pablo Guerrero <69773741+juanyugo99@users.noreply.github.com>
2021-03-11 20:17:05 -0500

    Update mensajetxt.py

--- Grupo1_encriptacionCesar/mensajetxt.py
+++ Grupo1_encriptacionCesar/mensajetxt.py
@@ -1,5 +1,8 @@
 # función para escribir el mensaje cifrado en un archivo texto
 # parametros de entrada:    mensaje - mensaje encriptado en string
 def mensaje_a_txt(mensaje_cifrado):  # funcion 5
+    f = open("mensaje_cifrado_grupo1.txt","w+")
+    f.write(str(mensaje_cifrado))
+    f.close()
     # no tiene retorno
     # el archivo texto debe llamarse mensaje_cifrado_grupo1.txt
