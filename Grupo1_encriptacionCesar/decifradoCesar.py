e365e58cab820004ffcf38bac400a9406dfc4682
chechodez <79472700+chechodez@users.noreply.github.com>
2021-03-11 20:06:13 -0500

    Update decifradoCesar.py

--- Grupo1_encriptacionCesar/decifradoCesar.py
+++ Grupo1_encriptacionCesar/decifradoCesar.py
@@ -2,4 +2,7 @@
 # parametros de entrada:    tasa - es la tasa que se usara en la encriptación
 #                           mensaje_lista - el mensaje a desencriptar en modo de lista
 def decifrado_cesar(tasa, mensaje_cifrado):  # funcion 8
-    return # retorne el mensaje decifrado
+    tamaño=len(mensaje_cifrado)
+    for x in range(tamaño):
+        mesaje_descifrado[x]=mesaje_cifrado[x]+tasa
+    return mensaje_descifrado# retorne el mensaje decifrado
