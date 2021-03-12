cb92b96d96999a18f010aa8289faa77aa1e37747
danielOva365 <daniovalle001@gmail.com>
2021-03-11 19:58:42 -0500

    Update seleccionNumero.py

--- Grupo1_encriptacionCesar/seleccionNumero.py
+++ Grupo1_encriptacionCesar/seleccionNumero.py
@@ -1,4 +1,12 @@
 # función para seleccionar el numero que se usara para la tasa de encriptación
 # Ingresar un nmero entre 1 y 25, que no se permitan los demas
 def seleccionar_numero():  # funcion 3
-    return # retornar el numero seleccionado
+    Num=input("Ingrese el número que desea usar de encripatción\n")
+    Num=int(Num)
+    
+    while Num > 25 or Num < 1:
+        print("Ingrese un número entre 1 y 25")
+        Num=input("Ingrese el número que desea usar de encripatción:\n")
+        Num=int(Num)
+        
+    return Num
