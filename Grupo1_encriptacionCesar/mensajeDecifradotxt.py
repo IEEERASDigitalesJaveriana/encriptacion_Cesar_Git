12bfc9cdffc822dfec81347b70b758086c7c6ac6
marinho14 <70497951+marinho14@users.noreply.github.com>
2021-03-11 19:46:54 -0500

    Update mensajeDecifradotxt.py

--- Grupo1_encriptacionCesar/mensajeDecifradotxt.py
+++ Grupo1_encriptacionCesar/mensajeDecifradotxt.py
@@ -5,4 +5,4 @@ def mensaje_decifrado_a_txt(mensaje_decifrado):  # funcion 10
    texto_t.close()
    texto_t=open("mensaje_decifrado_grupo1.txt","a")
    texto_t.write("El mensaje decifrado es:  " + mensaje_decifrado)
-   texto_t.close()
+   texto_t.close()##
