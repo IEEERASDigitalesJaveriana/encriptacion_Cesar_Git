b16930d4c176ab12c30bde544303a5d295b7e4d1
marinho14 <70497951+marinho14@users.noreply.github.com>
2021-03-11 19:40:33 -0500

    Update mensajeDecifradotxt.py

--- Grupo1_encriptacionCesar/mensajeDecifradotxt.py
+++ Grupo1_encriptacionCesar/mensajeDecifradotxt.py
@@ -1,5 +1,8 @@
 # función para escribir el mensaje decifrado en un archivo texto
 # parametros de entrada:    mensaje_decifrado - mensaje desencriptado en string
 def mensaje_decifrado_a_txt(mensaje_decifrado):  # funcion 10
-    # no tiene retorno
-    # el archivo texto debe llamarse mensaje_decifrado_grupo1.txt
+   texto_t=open("mensaje_decifrado_grupo1.txt","w")
+   texto_t.close()
+   texto_t=open("SEBASTIAN_MARINO.txt","a")
+   texto_t.write("El mensaje decifrado es:  " + mensaje_decifrado)
+   texto_t.close()
