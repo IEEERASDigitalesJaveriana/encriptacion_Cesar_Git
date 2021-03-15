a61b06d6cbae8752e0f5be3acbcf33cdf3e1f96d
Lorena Castillo <jessica.castillo@javeriana.edu.co>
2021-03-14 21:46:56 -0500

    se cambio la funcion cifrado

--- Grupo2_encriptacionCesar/cifradoCesar.py
+++ Grupo2_encriptacionCesar/cifradoCesar.py
@@ -2,4 +2,13 @@
 # parametros de entrada:    tasa - es la tasa que se usara en la encriptación
 #                           mensaje_lista - el mensaje a encriptar en modo de lista
 def cifrado_cesar(tasa, mensaje_lista):  # funcion 4
-    return # retornar el mensaje cifrado en string
+    mensaje_lista = ""
+    msg = upper(mensaje_lista)
+    for char in msg:
+        if char.isalpha():
+            cifrado = ord(char) + tasa
+            if char > 'Z':
+                cifrado = cifrado -ord('Z') + (ord('A')-1)
+        else:
+            mensaje +=cifrado
+    return mensaje# retornar el mensaje cifrado en string
