3ed803951842338ec467999396fd58dc5d29bbba
Juan Sebastián Barreto Jiménez <juan_barreto@javeriana.edu.co>
2021-09-11 13:20:34 -0500

    Agregue print

--- Grupo1_encriptacionCesar/cifradoCesar.py
+++ Grupo1_encriptacionCesar/cifradoCesar.py
@@ -2,4 +2,5 @@
 # parametros de entrada:    tasa - es la tasa que se usara en la encriptación
 #                           mensaje_lista - el mensaje a encriptar en modo de lista
 def cifrado_cesar(tasa, mensaje_lista):  # funcion 4
+    print("Hola")
     return # retornar el mensaje cifrado en string
